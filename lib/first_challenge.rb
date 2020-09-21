42a713bf1901d2d65c26422d68486485a33207df
thoughtful-storage-6584 <csimon1127@gmail.com>
2020-09-21 17:01:58 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,3 +1,3 @@
 def first_challenge
-   epic_tragedy = {"Montague" => {:patriarch {}, :matriarch {}, :hero {}, }, "Capulet" => {}}
+   epic_tragedy = {"Montague" => {:patriarch => {}, :matriarch => {}, :hero => {}, :hero_friends => []}, "Capulet" => {:patriarch => {}, :matriarch => {}, :heroine => {}, :heroine_friends => []}}
 end
